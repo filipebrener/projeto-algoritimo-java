e86dcddd2175e0412db060cdc01d05941672a626
Filipe Brener <filipe@itexto.com.br>
2023-09-27 13:17:53 -0300

    ajuste no SelectionSort

--- src/domain/algoritimos/SelectionSort.java
+++ src/domain/algoritimos/SelectionSort.java
@@ -7,19 +7,15 @@ public class SelectionSort extends Algoritmo {
         super("Selection Sort");
     }
 
-    @Override
-    protected int[] ordernar(int[] vetor) {
-        int n = vetor.length;
-        for (int i = 0; i < n - 1; i++) {
-            // Encontra o índice do menor elemento no subarray não ordenado
+    public int[] ordernar(int[] vetor) {
+        for (int i = 0; i < vetor.length - 1; i++) {
             int minIndex = i;
-            for (int j = i + 1; j < n; j++) {
+            for (int j = i + 1; j < vetor.length; j++) {
                 if (vetor[j] < vetor[minIndex]) {
                     minIndex = j;
                 }
             }
 
-            // Troca o menor elemento com o primeiro elemento não ordenado
             int temp = vetor[i];
             vetor[i] = vetor[minIndex];
             vetor[minIndex] = temp;
